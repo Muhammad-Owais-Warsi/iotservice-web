174887fb2c1476758ba89c59b7fbc57806a9d605
Naveen pandian <npandian515@gmail.com>
2025-12-16 22:20:47 +0530

    code cleanup

--- web/backend/index.js
+++ web/backend/index.js
@@ -1,1 +1,43 @@
-console.log("Hello via Bun!");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const authRoutes = require("./routes/authRoutes");
+
+
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
+
+
+app.use(
+    "/api",
+    authRoutes,
+);
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("database connected");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
