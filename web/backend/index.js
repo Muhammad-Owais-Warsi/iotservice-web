fedbdf11f5d77a543c44a6228176f907d43e1edf
Naveen pandian <npandian515@gmail.com>
2025-12-24 11:27:19 +0530

    add validation

--- web/backend/index.js
+++ web/backend/index.js
@@ -29,6 +29,7 @@ app.use(express.json({
     }
 }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use('/uploads', express.static('uploads'));
 
 // ============================================
 // SOCKET.IO CONNECTION
